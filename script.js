3cb76342409e76a30b879aa0a92b131109fdedbd
Alek Groove <sever.rec@gmail.com>
2021-04-04 02:22:26 +1200

    svg fix and js format

--- script.js
+++ script.js
@@ -8,29 +8,29 @@ const sideBarWide = document.querySelector('.sidebar__wide')
 
 // Events
 burgerIcon.addEventListener('click', function () {
-    burgerMenu.style.display = 'block';
+  burgerMenu.style.display = 'block';
 })
 
 burgerClose.addEventListener('click', function () {
-    burgerMenu.style.display = 'none';
+  burgerMenu.style.display = 'none';
 })
 
 burgerItems.addEventListener('click', function (e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    if (e.target.classList.contains('sidebar__link')) {
-        const id = e.target.getAttribute('href');
-        document.querySelector(id).scrollIntoView({ behavior: 'smooth' })
-    }
+  if (e.target.classList.contains('sidebar__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' })
+  }
 })
 
 sideBarWide.addEventListener('click', function (e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    if (e.target.classList.contains('sidebar__link')) {
-        const id = e.target.getAttribute('href');
-        document.querySelector(id).scrollIntoView({ behavior: 'smooth' })
-    }
+  if (e.target.classList.contains('sidebar__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' })
+  }
 })
 
 //Tabbed component
@@ -40,20 +40,20 @@ const tabsContent = document.querySelectorAll('.advantages--description');
 const tabsLines = document.querySelectorAll('.advantages--description-line');
 
 tabsContainer.addEventListener('click', function (e) {
-    const clicked = e.target.closest('.adv-buttons--btn');
+  const clicked = e.target.closest('.adv-buttons--btn');
 
-    if (clicked) {
-        tabs.forEach(t => t.classList.remove('adv-buttons--active'));
-        tabsContent.forEach(description => description.classList.remove('advantages--description-active'));
-        tabsLines.forEach(line => line.classList.remove('advantages--description-line--active'))
+  if (clicked) {
+    tabs.forEach(t => t.classList.remove('adv-buttons--active'));
+    tabsContent.forEach(description => description.classList.remove('advantages--description-active'));
+    tabsLines.forEach(line => line.classList.remove('advantages--description-line--active'))
 
-        clicked.classList.add('adv-buttons--active');
+    clicked.classList.add('adv-buttons--active');
 
-        document.querySelector(`.advantages--description-${clicked.dataset.tab}`).classList.add('advantages--description-active');
+    document.querySelector(`.advantages--description-${clicked.dataset.tab}`).classList.add('advantages--description-active');
 
-        document.querySelector(`.advantages--description-line-${clicked.dataset.tab}`).classList.add('advantages--description-line--active')
+    document.querySelector(`.advantages--description-line-${clicked.dataset.tab}`).classList.add('advantages--description-line--active')
 
-    }
+  }
 
 })
 
@@ -61,79 +61,79 @@ tabsContainer.addEventListener('click', function (e) {
 const footer = document.querySelector('.footer')
 
 const footerFocus = function (e, opacity) {
-    if (e.target.classList.contains('options__link')) {
-        const link = e.target;
-        const siblings = link.closest('.options').querySelectorAll('.options__link');
+  if (e.target.classList.contains('options__link')) {
+    const link = e.target;
+    const siblings = link.closest('.options').querySelectorAll('.options__link');
 
-        siblings.forEach(s => {
-            if (s !== link) s.style.opacity = opacity;
-        })
-    }
+    siblings.forEach(s => {
+      if (s !== link) s.style.opacity = opacity;
+    })
+  }
 }
 
 footer.addEventListener('mouseover', function (e) {
-    footerFocus(e, 0.5)
+  footerFocus(e, 0.5)
 })
 footer.addEventListener('mouseout', function (e) {
-    footerFocus(e, 1)
+  footerFocus(e, 1)
 })
 // Mouseover burgerMenu
 const sidebarItems = document.querySelector('.sidebar__open');
 
 const burgerItemFocus = function (e, opacity) {
-    if (e.target.classList.contains('sidebar__link')) {
-        const link = e.target;
-        const siblings = link.closest('.sidebar__open').querySelectorAll('.sidebar__link');
+  if (e.target.classList.contains('sidebar__link')) {
+    const link = e.target;
+    const siblings = link.closest('.sidebar__open').querySelectorAll('.sidebar__link');
 
-        siblings.forEach(s => {
-            if (s !== link) s.style.opacity = opacity;
-        })
-    }
+    siblings.forEach(s => {
+      if (s !== link) s.style.opacity = opacity;
+    })
+  }
 }
 
 sidebarItems.addEventListener('mouseover', function (e) {
-    burgerItemFocus(e, 0.5)
+  burgerItemFocus(e, 0.5)
 })
 sidebarItems.addEventListener('mouseout', function (e) {
-    burgerItemFocus(e, 1)
+  burgerItemFocus(e, 1)
 })
 //Mouseover sidebar
 const sidebarFocus = function (e, opacity) {
-    if (e.target.classList.contains('sidebar__link')) {
-        const link = e.target;
-        const siblings = link.closest('.sidebar__wide').querySelectorAll('.sidebar__link');
+  if (e.target.classList.contains('sidebar__link')) {
+    const link = e.target;
+    const siblings = link.closest('.sidebar__wide').querySelectorAll('.sidebar__link');
 
-        siblings.forEach(s => {
-            if (s !== link) s.style.opacity = opacity;
-        })
-    }
+    siblings.forEach(s => {
+      if (s !== link) s.style.opacity = opacity;
+    })
+  }
 }
 
 sideBarWide.addEventListener('mouseover', function (e) {
-    sidebarFocus(e, 0.5)
+  sidebarFocus(e, 0.5)
 })
 sideBarWide.addEventListener('mouseout', function (e) {
-    sidebarFocus(e, 1)
+  sidebarFocus(e, 1)
 })
 
 //Revealing elements 
 const sectionsAll = document.querySelectorAll('.section');
 
 const revealSection = function (entries, observer) {
-    const [entry] = entries;
+  const [entry] = entries;
 
-    if (!entry.isIntersecting) return;
+  if (!entry.isIntersecting) return;
 
-    entry.target.classList.remove('section--hidden');
-    observer.unobserve(entry.target)
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target)
 }
 
 const sectionObserver = new IntersectionObserver(revealSection, {
-    root: null,
-    threshold: 0.10,
+  root: null,
+  threshold: 0.10,
 })
 
 sectionsAll.forEach(section => {
-    sectionObserver.observe(section)
-    section.classList.add('section--hidden')
+  sectionObserver.observe(section)
+  section.classList.add('section--hidden')
 })
